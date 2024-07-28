55ab2625635da0f99d7d6125bf79a2a9372ca254
Gregorius Yuristama <gregoriusyuristama@gmail.com>
2024-07-28 08:15:00 +0700

    add ai integratiom

--- lib/Features/Chat/View/chat_screen.dart
+++ lib/Features/Chat/View/chat_screen.dart
@@ -1,8 +1,11 @@
 import 'package:c_enam/Core/Constant/resource_path.dart';
+import 'package:c_enam/Core/Utilities/gemini_utilities.dart';
 import 'package:c_enam/Features/Chat/Components/prompt_card.dart';
 import 'package:c_enam/Features/Chat/Components/prompt_card_list.dart';
 import 'package:c_enam/Features/Chat/Components/send_prompt_card.dart';
 import 'package:c_enam/Features/Chat/Components/welcome_view.dart';
+import 'package:c_enam/Features/Chat/Controller/merchant_controller.dart';
+import 'package:c_enam/Features/Chat/Model/merchant_model.dart';
 import 'package:c_enam/Features/Chat/Model/message_model.dart';
 import 'package:c_enam/Features/Chat/Model/prompt_model.dart';
 import 'package:flutter/material.dart';
@@ -47,6 +50,7 @@ class _ChatScreenState extends State<ChatScreen> {
     _textController.dispose();
   }
 
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
     messageProvider = Provider.of<ChatMessageProvider>(context, listen: true);
@@ -73,81 +77,19 @@ class _ChatScreenState extends State<ChatScreen> {
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           Flexible(
-            child: SingleChildScrollView(
-              child: Consumer<ChatMessageProvider>(
-                builder: (context, value, child) => value.messages.isEmpty
-                    ? WelcomeView()
-                    : ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: value.messages.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 24, vertical: 8),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                if (value.messages[index].isSelf)
-                                  const Spacer(),
-                                if (!value.messages[index].isSelf)
-                                  ClipOval(
-                                    child: Container(
-                                      padding: EdgeInsets.only(
-                                        top: 16,
-                                        right: 16,
-                                      ),
-                                      width: 20,
-                                      height: 20,
-                                      color: CEcolors.green,
-                                    ),
-                                  ),
-                                Flexible(
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        margin: EdgeInsets.only(
-                                          left: 8,
-                                        ),
-                                        padding: EdgeInsets.symmetric(
-                                          vertical: 16,
-                                          horizontal: 12,
-                                        ),
-                                        decoration: BoxDecoration(
-                                          color: value.messages[index].isSelf
-                                              ? CEcolors.greyCard
-                                              : CEcolors.greenChatCard,
-                                          borderRadius: value
-                                                  .messages[index].isSelf
-                                              ? BorderRadius.only(
-                                                  topLeft: Radius.circular(12),
-                                                  bottomLeft:
-                                                      Radius.circular(12),
-                                                  bottomRight:
-                                                      Radius.circular(12),
-                                                )
-                                              : BorderRadius.only(
-                                                  topRight: Radius.circular(12),
-                                                  bottomLeft:
-                                                      Radius.circular(12),
-                                                  bottomRight:
-                                                      Radius.circular(12),
-                                                ),
-                                        ),
-                                        child: Text(
-                                          value.messages[index].message,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-              ),
+            child: Consumer<ChatMessageProvider>(
+              builder: (context, value, child) => value.messages.isEmpty
+                  ? const WelcomeView()
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: value.messages.length,
+                      itemBuilder: (context, index) {
+                        return chatContainer(value, index);
+                      },
+                    ),
             ),
           ),
+          isLoading ? CircularProgressIndicator.adaptive() : SizedBox(),
           Container(
             padding: EdgeInsets.only(
               bottom: MediaQuery.of(context).padding.bottom,
@@ -169,7 +111,7 @@ class _ChatScreenState extends State<ChatScreen> {
               child: Container(
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.all(
+                  borderRadius: const BorderRadius.all(
                     Radius.circular(12),
                   ),
                 ),
@@ -178,7 +120,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     Flexible(
                       child: TextField(
                         controller: _textController,
-                        decoration: InputDecoration(
+                        decoration: const InputDecoration(
                           hintText: "What do you want to eat?",
                           border: InputBorder.none,
                           contentPadding: EdgeInsets.only(left: 16),
@@ -191,13 +133,14 @@ class _ChatScreenState extends State<ChatScreen> {
                             child: IconButton(
                               style: IconButton.styleFrom(
                                 backgroundColor: Colors.green,
-                                shape: RoundedRectangleBorder(
+                                shape: const RoundedRectangleBorder(
                                   side: BorderSide(width: 0),
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(8)),
                                 ),
                               ),
-                              onPressed: () {
+                              onPressed: () async {
+                                String prompt = _textController.text;
                                 messageProvider?.appendMessage(
                                   Message(
                                     message: _textController.text,
@@ -206,22 +149,95 @@ class _ChatScreenState extends State<ChatScreen> {
                                 );
                                 _textController.clear();
                                 FocusManager.instance.primaryFocus?.unfocus();
+                                isLoading = true;
+                                RespMerchants resp =
+                                    await MerchantController.fetchMerchants();
+                                String answer =
+                                    await GeminiUtilites.shared.askFood(
+                                  prompt,
+                                  resp.data ?? [],
+                                );
+                                isLoading = false;
+                                messageProvider?.appendMessage(
+                                  Message(
+                                    message: answer,
+                                    isSelf: false,
+                                  ),
+                                );
                               },
                               icon: SvgPicture.asset(
                                 ResourcePath.sendIcon,
-                                colorFilter: ColorFilter.mode(
+                                colorFilter: const ColorFilter.mode(
                                   Colors.white,
                                   BlendMode.srcIn,
                                 ),
                               ),
                             ),
                           )
-                        : SizedBox(),
+                        : const SizedBox(),
                   ],
                 ),
               ),
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget chatContainer(ChatMessageProvider value, int index) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (value.messages[index].isSelf) const Spacer(),
+          if (!value.messages[index].isSelf)
+            ClipOval(
+              child: Container(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  right: 16,
+                ),
+                width: 20,
+                height: 20,
+                color: CEcolors.green,
+              ),
+            ),
+          Flexible(
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 8,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    color: value.messages[index].isSelf
+                        ? CEcolors.greyCard
+                        : CEcolors.greenChatCard,
+                    borderRadius: value.messages[index].isSelf
+                        ? const BorderRadius.only(
+                            topLeft: Radius.circular(12),
+                            bottomLeft: Radius.circular(12),
+                            bottomRight: Radius.circular(12),
+                          )
+                        : const BorderRadius.only(
+                            topRight: Radius.circular(12),
+                            bottomLeft: Radius.circular(12),
+                            bottomRight: Radius.circular(12),
+                          ),
+                  ),
+                  child: Text(
+                    value.messages[index].message,
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
